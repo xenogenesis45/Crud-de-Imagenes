bae7772738cfab82267be32ccd8eb157bb773f79
xenogenesis45 <xenogenesis45@gmail.com>
2021-08-12 12:22:11 -0500

    crud de imagenes con node, react y mysql con typescript

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,20 @@
 import React, { Fragment, ChangeEvent, useState, useEffect } from 'react';
+import Modal from 'react-modal'
 
 type InputChange = ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
 
 function App() {
+
 
   const [fileArch, setFileArch] = useState(null)
 
   const [imageList, setimageList] = useState([])
 
   const [listUpdate, setlistUpdate] = useState(false)
+
+  const [modalIsOpen, setmodalIsOpen] = useState(false)
+
+  const [currentImage, setcurrentImage] = useState(null)
 
   const selectHandler = async (e: InputChange) => {
     const input = e.target as HTMLInputElement;
@@ -49,7 +55,26 @@ function App() {
     // setFileArch(null)
   }
 
+  const deleteHandler = (currentImage: any) => {
+    let imageId = currentImage.split('-')
+    console.log(imageId[0]);
+    imageId = parseInt(imageId[0])
+
+    fetch('http://localhost:5000/images/delete/' + imageId, {
+      method: 'DELETE'
+    })
+      .then(res => res.text())
+      .then(res => {
+        setlistUpdate(true)
+        console.log(res)
+      })
+    setmodalIsOpen(false)
+
+  }
+
   useEffect(() => {
+    Modal.setAppElement('body');
+
     fetch('http://localhost:5000/images/get')
       .then(res => res.json())
       .then(res => setimageList(res))
@@ -59,6 +84,10 @@ function App() {
     setlistUpdate(false)
   }, [listUpdate])
 
+  const modalHadler = (isOpen: any, image: any) => {
+    setmodalIsOpen(isOpen)
+    setcurrentImage(image)
+  }
 
   return (
     <Fragment>
@@ -87,11 +116,21 @@ function App() {
           <div key={image} className='card m-2' >
             <img src={'http://localhost:5000/' + image} alt="..." className='img-card-top' style={{ height: '200px', width: '300px' }} />
             <div className='card-body' style={{ textAlign: 'center' }}>
-              <button className='btn btn-dark'>Click para ver</button>
+              <button className='btn btn-dark' onClick={() => modalHadler(true, image)}>Click para ver</button>
             </div>
           </div>
         ))}
       </div>
+
+      <Modal isOpen={modalIsOpen} onRequestClose={() => modalHadler(false, null)} style={{ content: { right: '20%', left: '20%' } }}>
+        <div className='card'>
+          <img src={'http://localhost:5000/' + currentImage} alt="...." />
+          <div className='card-body' style={{ textAlign: 'center' }}>
+            <button className='btn btn-danger' onClick={() => deleteHandler(currentImage)}>Eliminar</button>
+          </div>
+        </div>
+      </Modal>
+
     </Fragment>
   );
 }
